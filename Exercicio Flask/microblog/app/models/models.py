d7371144792f490fcdfe23c5b78f748073c30a66
unknown <Admin@ITAURB-052.itaurb.local>
2024-11-19 13:51:24 -0300

    Adicionado modelo Post

--- Exercicio Flask/microblog/app/models/models.py
+++ Exercicio Flask/microblog/app/models/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from app import db
 from flask_login import UserMixin
 from app import login
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -12,7 +14,17 @@ class User(UserMixin,db.Model):
     profile_picture: Mapped[str] = mapped_column(nullable=True)
     bio: Mapped[str] = mapped_column(nullable=True)
     remember: Mapped[bool] = mapped_column(default=False)
-    last_login: Mapped[datetime] = mapped_column(nullable=True) 
+    last_login: Mapped[datetime] = mapped_column(nullable=True)
+    posts: Mapped[list['Post']] = relationship(back_populates = 'author') 
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    body: Mapped[str] = mapped_column()
+    timestamp: Mapped[datetime] = mapped_column(nullable=True) 
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    author: Mapped[User] = relationship(back_populates='posts')
+
 
 @login.user_loader
 def load_user(id):
